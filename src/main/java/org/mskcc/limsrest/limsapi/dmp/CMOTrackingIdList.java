cb1a714239af5752a2af4f969c7b6cf37fb4c7fb
imprezka88 <imprezka88@gmail.com>
2017-11-28 10:12:10 -0500

    [IGOWEB-858] Add missing classes

---
+++ src/main/java/org/mskcc/limsrest/limsapi/dmp/CMOTrackingIdList.java
@@ -0,0 +1,76 @@
+package org.mskcc.limsrest.limsapi.dmp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CMOTrackingIdList {
+    private String runDate;
+    private String result;
+    private Content content;
+
+    public CMOTrackingIdList() {
+    }
+
+    public String getRunDate() {
+        return runDate;
+    }
+
+    public void setRunDate(String runDate) {
+        this.runDate = runDate;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public Content getContent() {
+        return content;
+    }
+
+    public void setContent(Content content) {
+        this.content = content;
+    }
+
+    public List<String> getTrackingIds() {
+        return content.getTrackingIds();
+    }
+
+    @Override
+    public String toString() {
+        return "CmoTrackingIdList{" +
+                "runDate='" + runDate + '\'' +
+                ", result='" + result + '\'' +
+                ", content=" + content +
+                '}';
+    }
+
+    private class Content {
+        @JsonProperty("TrackingId List")
+        private List<String> trackingIds;
+
+        public Content() {
+        }
+
+        public List<String> getTrackingIds() {
+            return trackingIds;
+        }
+
+        public void setTrackingIds(List<String> trackingIds) {
+            this.trackingIds = trackingIds;
+        }
+
+        @Override
+        public String toString() {
+            return "Content{" +
+                    "trackingIds=" + trackingIds +
+                    '}';
+        }
+    }
+}
