c926811b989b578739b74b5f5d8fd5c6eafcee66
David McManamon <dmcmanam@gmail.com>
2018-06-18 10:30:03 -0400

    Patterns all static final and access methods static too

--- src/main/java/org/mskcc/limsrest/web/Whitelists.java
+++ src/main/java/org/mskcc/limsrest/web/Whitelists.java
@@ -7,86 +7,66 @@ import java.util.regex.Pattern;
   I'm hopeful the lims is resistant to attack vectors this way, but I haven't tested
 */
 public class Whitelists {
-    // TODO static final naming conventions
-    private static final Pattern requestPattern = Pattern.compile("^[0-9]{5,}[A-Z_]*$");
-    public static final Pattern sampleNamePattern = Pattern.compile("^[A-Za-z0-9_-]+$");
-    private static final Pattern genericParamPattern = Pattern.compile("^[ A-Za-z0-9_'.+-]+$");
-    private static final Pattern picklistPattern = Pattern.compile("^[A-Za-z0-9 ()/-]+$");
-    private static final Pattern filePathPattern = Pattern.compile("^[A-Za-z0-9_/.-]*$");
-    private static final Pattern servicePattern = Pattern.compile("^(IGO|igo|iGO|iGo)[-_][0-9]{4,8}$");
-    private static final Pattern specialParamPattern = Pattern.compile("^[ A-Za-z0-9_'().+-]+$");
+    private static final Pattern REQUEST_PATTERN = Pattern.compile("^[0-9]{5,}[A-Z_]*$");
+    public  static final Pattern SAMPLE_NAME_PATTERN = Pattern.compile("^[A-Za-z0-9_-]+$");
+    private static final Pattern GENERIC_PARAM_PATTERN = Pattern.compile("^[ A-Za-z0-9_'.+-]+$");
+    private static final Pattern FILE_PATH_PATTERN = Pattern.compile("^[A-Za-z0-9_/.-]*$");
+    private static final Pattern SERVICE_PATTERN = Pattern.compile("^(IGO|igo|iGO|iGo)[-_][0-9]{4,8}$");
+    private static final Pattern SPECIAL_PARAM_PATTERN = Pattern.compile("^[ A-Za-z0-9_'().+-]+$");
 
-    public Whitelists() {
-    }
-
-    public String serviceFormatText() {
+    public static String serviceFormatText() {
         return "The service id must be igo  followed by a dash or hyphen followed by numbers";
     }
 
-    public boolean serviceMatches(String service) {
+    public static boolean serviceMatches(String service) {
         if (service == null || service.equals("NULL"))
             return true;
-        return servicePattern.matcher(service).matches(); //banked requests can have "igo/IGO prefix
+        return SERVICE_PATTERN.matcher(service).matches(); //banked requests can have "igo/IGO prefix
     }
 
-    public String requestFormatText() {
+    public static String requestFormatText() {
         return "The format must be five digits followed an optional underscore and capital letter";
     }
 
     public static boolean requestMatches(String request) {
         if (request == null || request.equals("NULL"))
             return true;
-        return requestPattern.matcher(request).matches();
+        return REQUEST_PATTERN.matcher(request).matches();
     }
 
-    public String filePathFormatText() {
+    public static String filePathFormatText() {
         return "The format must be a valid unix file path";
     }
 
-    public boolean filePathMatches(String path) {
+    public static boolean filePathMatches(String path) {
         if (path == null || path.equals("NULL"))
             return true;
-        return filePathPattern.matcher(path).matches();
+        return FILE_PATH_PATTERN.matcher(path).matches();
     }
 
-    public String sampleFormatText() {
+    public static String sampleFormatText() {
         return "The format must only contain upper case and lower case letters, numbers, underscores and dashes";
-
     }
 
-    public boolean sampleMatches(String sample) {
+    public static boolean sampleMatches(String sample) {
         if (sample == null || sample.equals("NULL"))
             return true;
-        return sampleNamePattern.matcher(sample).matches();
+        return SAMPLE_NAME_PATTERN.matcher(sample).matches();
     }
 
-    public String picklistFormatText() {
-        return "The format must only contain upper case and lower case letters, numbers, spaces, parenthesis, slashes and dashes";
-    }
-
-    public boolean picklistMatches(String list) {
-        if (list == null || list.equals("NULL"))
-            return true;
-        return picklistPattern.matcher(list).matches();
-    }
-
-    public String textFormatText() {
+    public static String textFormatText() {
         return "The format must only contain upper case and lower case letters, numbers, underscores, single quotes, periods, plusses and minuses";
     }
 
-    public boolean textMatches(String text) {
+    public static boolean textMatches(String text) {
         if (text == null || text.equals("NULL"))
             return true;
-        return genericParamPattern.matcher(text).matches();
+        return GENERIC_PARAM_PATTERN.matcher(text).matches();
     }
 
-    public String specialFormatText() {
-        return "The format must only contain upper case and lower case letters, numbers, underscores, single quotes, periods, plusses and minuses and params";
-    }
-
-    public boolean specialMatches(String text) {
+    public static boolean specialMatches(String text) {
         if (text == null || text.equals("NULL"))
             return true;
-        return specialParamPattern.matcher(text).matches();
+        return SPECIAL_PARAM_PATTERN.matcher(text).matches();
     }
 }
