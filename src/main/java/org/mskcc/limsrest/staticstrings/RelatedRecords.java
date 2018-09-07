263f0e8eca2734418652a187409eb6c647c2fe39
David McManamon <dmcmanam@gmail.com>
2018-09-07 09:25:16 -0400

    RelatedRecords.java removed.

--- src/main/java/org/mskcc/limsrest/staticstrings/RelatedRecords.java
+++
@@ -1,9 +0,0 @@
-package org.mskcc.limsrest.staticstrings;
-
-public interface RelatedRecords{
-    public static final String MICRONIC_TUBE_REF = "RelatedRecord321";
-    public static final String MICRONIC_RACK = "RelatedRecord366";
-    public static final String CFDNA_LAB_MEDICINE = "RelatedRecord464";
-    public static final String PLATE_REF = "RelatedRecord23";
-    public static final String FLOWCELL_REF = "RelatedRecord134";
-}
