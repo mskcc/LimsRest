83c7d6a378f41050b0b36626aaf26efc8c4ef439
David McManamon <dmcmanam@gmail.com>
2018-12-17 14:09:26 -0500

    PM team delivery email fields retrieved from LIMS new endpoint

---
+++ src/main/java/org/mskcc/limsrest/limsapi/DeliveryEmail.java
@@ -0,0 +1,71 @@
+package org.mskcc.limsrest.limsapi;
+
+public class DeliveryEmail {
+    String projectTitle;
+    String requestIDLIMS;
+    String mergeID;
+    String investigatorEmail;
+    String sequencingApplication; // display name "Project Applications"
+
+    String errorMessage = null;
+
+    public DeliveryEmail(String requestIDLIMS, String projectTitle, String mergeID, String investigatorEmail, String sequencingApplication) {
+        this.projectTitle = projectTitle;
+        this.requestIDLIMS = requestIDLIMS;
+        this.mergeID = mergeID;
+        this.investigatorEmail = investigatorEmail;
+        this.sequencingApplication = sequencingApplication;
+    }
+
+    public DeliveryEmail(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getProjectTitle() {
+        return projectTitle;
+    }
+
+    public void setProjectTitle(String projectTitle) {
+        this.projectTitle = projectTitle;
+    }
+
+    public String getRequestIDLIMS() {
+        return requestIDLIMS;
+    }
+
+    public void setRequestIDLIMS(String requestIDLIMS) {
+        this.requestIDLIMS = requestIDLIMS;
+    }
+
+    public String getMergeID() {
+        return mergeID;
+    }
+
+    public void setMergeID(String mergeID) {
+        this.mergeID = mergeID;
+    }
+
+    public String getInvestigatorEmail() {
+        return investigatorEmail;
+    }
+
+    public void setInvestigatorEmail(String investigatorEmail) {
+        this.investigatorEmail = investigatorEmail;
+    }
+
+    public String getSequencingApplication() {
+        return sequencingApplication;
+    }
+
+    public void setSequencingApplication(String sequencingApplication) {
+        this.sequencingApplication = sequencingApplication;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
---
+++ src/main/java/org/mskcc/limsrest/limsapi/GetDeliveryEmailDetails.java
@@ -0,0 +1,50 @@
+package org.mskcc.limsrest.limsapi;
+
+import com.velox.api.datarecord.DataRecord;
+import com.velox.sapioutils.client.standalone.VeloxConnection;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+
+@Service
+public class GetDeliveryEmailDetails extends LimsTask {
+    private Log log = LogFactory.getLog(GetDeliveryEmailDetails.class);
+    protected String requestID;
+
+    public void init(String requestID) {
+        this.requestID = requestID;
+    }
+
+    @Override
+    @PreAuthorize("hasRole('READ')")
+    public Object execute(VeloxConnection conn) {
+        try {
+            List<DataRecord> requests = dataRecordManager.queryDataRecords("Request", "RequestID='" + requestID +"'", this.user);
+            if (requests.size() != 1) {
+                throw new Exception("Request " + requestID + " not found.");
+            }
+            DataRecord request = requests.get(0);
+            DataRecord project = request.getParentsOfType("Project", this.user).get(0);
+
+            String projectTitle = project.getStringVal("CMOFinalProjectTitle", this.user);
+            String requestIDLIMS = request.getStringVal("ProjectId", this.user);
+            String mergeID = project.getStringVal("CMOProjectID", this.user);
+            String investigatorEmail = request.getStringVal("Investigatoremail", this.user);
+            String sequencingApplication = request.getStringVal("PlatformApplication", this.user); // display name "Project Applications"
+
+            return new DeliveryEmail(requestIDLIMS, projectTitle, mergeID, investigatorEmail, sequencingApplication);
+        } catch (Throwable e) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            log.info(e.getMessage());
+            log.info(sw.toString());
+            return new DeliveryEmail(e.getMessage());
+        }
+    }
+}
