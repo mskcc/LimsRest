0ec01c51962ab0b1a0c8b2a1b8416898d91235fc
David McManamon <dmcmanam@gmail.com>
2019-02-28 12:55:22 -0500

    No more GeneralStaticStrings.java class

--- src/main/java/org/mskcc/limsrest/limsapi/ListStudies.java
+++ src/main/java/org/mskcc/limsrest/limsapi/ListStudies.java
@@ -1,60 +1,60 @@
 package org.mskcc.limsrest.limsapi;
 
-
 import com.velox.api.datarecord.DataRecord;
 import com.velox.sapioutils.client.standalone.VeloxConnection;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.mskcc.limsrest.staticstrings.GeneralStaticStrings;
 
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Find all studies/projects and list them 
- *     */
+ */
 public class ListStudies extends LimsTask {
-  private Log log = LogFactory.getLog(ListStudies.class);
-  boolean cmoOnly =true; 
+    boolean cmoOnly = true;
+    private Log log = LogFactory.getLog(ListStudies.class);
 
-  public void init(String cmoOnly){
-    if("false".equals(cmoOnly.toLowerCase())){
-        this.cmoOnly = false; 
-    }
-  }
-
- //execute the velox call
- @Override
- public Object execute(VeloxConnection conn){
-// private void runProgram(User apiUser, DataRecordManager dataRecordManager) {
-   LinkedList<ProjectSummary> allProjects = new LinkedList<>();
-
-   try {
-     List<DataRecord> limsProjectList = dataRecordManager.queryDataRecords("Project", null, user);
-     for(DataRecord p: limsProjectList){
-        ProjectSummary ps = new ProjectSummary(p.getStringVal("ProjectId", user));
-        annotateProjectSummary(ps, p) ;
-        DataRecord[] requests = p.getChildrenOfType("Request", user);
-        for(int i = 0; i < requests.length; i++){
-          try{
-            String pm = "";
-            try{ pm = requests[i].getStringVal("ProjectManager", user); } catch(NullPointerException npe){}
-            if(!cmoOnly || (cmoOnly && (!pm.equals("") && !pm.equals(GeneralStaticStrings.NO_PM)))){
-               RequestSummary rs = new RequestSummary(requests[i].getStringVal("RequestId", user));     
-               rs.setRecordId(requests[i].getRecordId());
-               ps.addRequestSummary(rs);
-            }
-          } catch(NullPointerException npe){}
+    public void init(String cmoOnly) {
+        if ("false".equals(cmoOnly.toLowerCase())) {
+            this.cmoOnly = false;
         }
-        allProjects.add(ps);
-     }
-   } catch (Throwable e) {
-       log.info(e.getMessage());
-       ProjectSummary errorProject = new ProjectSummary("ERROR");
-       errorProject.setCmoProjectId(e.getMessage());
-       allProjects.add(errorProject);
-   }
- 
-    return allProjects;
-  }
+    }
+
+    @Override
+    public Object execute(VeloxConnection conn) {
+        LinkedList<ProjectSummary> allProjects = new LinkedList<>();
+
+        try {
+            List<DataRecord> limsProjectList = dataRecordManager.queryDataRecords("Project", null, user);
+            for (DataRecord p : limsProjectList) {
+                ProjectSummary ps = new ProjectSummary(p.getStringVal("ProjectId", user));
+                annotateProjectSummary(ps, p);
+                DataRecord[] requests = p.getChildrenOfType("Request", user);
+                for (int i = 0; i < requests.length; i++) {
+                    try {
+                        String pm = "";
+                        try {
+                            pm = requests[i].getStringVal("ProjectManager", user);
+                        } catch (NullPointerException npe) {
+                        }
+                        if (!cmoOnly || (cmoOnly && (!pm.equals("") && !pm.equals("NO PM")))) {
+                            RequestSummary rs = new RequestSummary(requests[i].getStringVal("RequestId", user));
+                            rs.setRecordId(requests[i].getRecordId());
+                            ps.addRequestSummary(rs);
+                        }
+                    } catch (NullPointerException npe) {
+                    }
+                }
+                allProjects.add(ps);
+            }
+        } catch (Throwable e) {
+            log.info(e.getMessage());
+            ProjectSummary errorProject = new ProjectSummary("ERROR");
+            errorProject.setCmoProjectId(e.getMessage());
+            allProjects.add(errorProject);
+        }
+
+        return allProjects;
+    }
 }
--- src/main/java/org/mskcc/limsrest/staticstrings/GeneralStaticStrings.java
+++
@@ -1,7 +0,0 @@
-package org.mskcc.limsrest.staticstrings;
-
-public interface GeneralStaticStrings {
-
- public static final String NO_PM = "NO PM";
-}
-
