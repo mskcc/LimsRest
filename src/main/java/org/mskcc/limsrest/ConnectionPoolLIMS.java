0ae15194dddc1326dec12207da80eb95ccb17643
David McManamon <dmcmanam@gmail.com>
2019-08-15 14:54:40 -0400

    added comment

--- src/main/java/org/mskcc/limsrest/ConnectionPoolLIMS.java
+++ src/main/java/org/mskcc/limsrest/ConnectionPoolLIMS.java
@@ -7,6 +7,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+/**
+ * LIMS connection pool with dedicated LIMS connections.
+ */
 public class ConnectionPoolLIMS {
     private static final ExecutorService executor = Executors.newFixedThreadPool(2);
 
