7ff7a2fd8fa283b8424060c2d29524ebfbe218b3
as114 <ajaypaulsharma@yahoo.co.in>
2019-10-25 17:45:07 -0400

    added logic to convert string to comma separated string

--- src/main/java/org/mskcc/limsrest/service/GetWESSampleDataTask.java
+++ src/main/java/org/mskcc/limsrest/service/GetWESSampleDataTask.java
@@ -13,7 +13,6 @@ import org.json.JSONObject;
 import org.mskcc.limsrest.service.sampletracker.WESSampleData;
 import org.springframework.stereotype.Service;
 
-import javax.xml.crypto.Data;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -379,7 +378,7 @@ public class GetWESSampleDataTask extends LimsTask {
                 }
             }
         }
-        return StringUtils.join(", ", new ArrayList<>(sequencerTypes));
+        return String.join(",", sequencerTypes);
    }
 
    private String getSequencerType(String machineName){
