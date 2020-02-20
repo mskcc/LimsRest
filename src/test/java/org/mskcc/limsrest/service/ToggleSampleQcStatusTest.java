bdba7bf930c9fe07e51ee2c240fcf41fa44ce941
David Streid <david.streid.nyc@gmail.com>
2020-02-20 16:52:07 -0500

    Removing custom/non-custom capture test distinctions

--- src/test/java/org/mskcc/limsrest/service/ToggleSampleQcStatusTest.java
+++ src/test/java/org/mskcc/limsrest/service/ToggleSampleQcStatusTest.java
@@ -18,49 +18,8 @@ import static org.junit.Assert.assertEquals;
 import static org.mskcc.util.VeloxConstants.SAMPLE;
 
 public class ToggleSampleQcStatusTest {
-
     @Test
-    public void customCaptureRepoolTest() throws Exception {
-        /**
-         * A custom Capture recipe will send its input sample, the seqQcRecord, to  QcStatusAwareProcessAssigner
-         *      sampleRecord -"is [SAMPLE] Parent Of"-> seqQcRecord
-         */
-        String recipe = "other";
-        DataRecord seqQcRecord = Mockito.mock(DataRecord.class);
-        DataRecord sampleRecord = Mockito.mock(DataRecord.class);
-
-        /* ToggleSampleQcStatus MOCKS/SPIES */
-        VeloxConnection conn = Mockito.mock(VeloxConnection.class);
-        DataRecordManager drmMock = Mockito.mock(DataRecordManager.class);
-        User userMock = Mockito.mock(User.class);
-        QcStatusAwareProcessAssigner processAssignerSpy = Mockito.spy(QcStatusAwareProcessAssigner.class);
-
-        /* DATA RECORD MOCKS/SPIES */
-        List<DataRecord> sampleParents = new ArrayList<>();
-        sampleParents.add(sampleRecord);
-
-        /* INITIALIZE TASK */
-        long recordId = 5457947;
-        ToggleSampleQcStatus task = new ToggleSampleQcStatus();
-        task.init(recordId, "Repool-Sample", "", null, null, "Seq", null, null, null, recipe);
-        task.dataRecordManager = drmMock;
-        task.user = userMock;
-        task.qcStatusAwareProcessAssigner = processAssignerSpy;
-        Mockito.when(task.dataRecordManager.querySystemForRecord(recordId, "SeqAnalysisSampleQC", task.user)).thenReturn(seqQcRecord);
-        task.execute(conn);
-
-        Mockito.verify(processAssignerSpy, Mockito.times(0)).assign(Mockito.eq(drmMock), Mockito.eq(userMock), Mockito.eq(sampleRecord), Mockito.any(QcStatus.class));
-        Mockito.verify(processAssignerSpy, Mockito.times(1)).assign(Mockito.eq(drmMock), Mockito.eq(userMock), Mockito.eq(seqQcRecord), Mockito.any(QcStatus.class));
-    }
-
-    @Test
-    public void standardCaptureRepoolTest() throws Exception {
-        /**
-         * A Standard Capture Recipe should send its child sample w/ the pooling protocol to be assigned the process
-         *      sampleRecord - "is [SAMPLE] Parent Of"-> seqQcRecord
-         *      sampleRecord - "is [SAMPLE] Parent Of"-> poolingProtocolRecord
-         */
-        String recipe = "HemePACT_v4";
+    public void repoolTest() throws Exception {
         DataRecord seqQcRecord = Mockito.mock(DataRecord.class);
         DataRecord sampleRecord = Mockito.mock(DataRecord.class);
         DataRecord poolingProtocolRecord = Mockito.mock(DataRecord.class);
@@ -81,12 +40,11 @@ public class ToggleSampleQcStatusTest {
         /* INITIALIZE TASK */
         long recordId = 5457947;
         ToggleSampleQcStatus task = new ToggleSampleQcStatus();
-        task.init(recordId, "Repool-Sample", "", null, null, "Seq", null, null, null, recipe);
+        task.init(recordId, "Repool-Sample", "", null, null, "Seq", null, null, null);
         task.dataRecordManager = drmMock;
         task.user = userMock;
         task.qcStatusAwareProcessAssigner = processAssignerSpy;
         Mockito.when(task.dataRecordManager.querySystemForRecord(recordId, "SeqAnalysisSampleQC", task.user)).thenReturn(seqQcRecord);
-        // Mockito.doNothing().when(processAssignerSpy).assign(Mockito.eq(drmMock), Mockito.eq(userMock),Mockito.any(DataRecord.class),Mockito.any(QcStatus.class));
         task.execute(conn);
 
         Mockito.verify(processAssignerSpy, Mockito.times(1)).assign(Mockito.eq(drmMock), Mockito.eq(userMock), Mockito.eq(sampleRecord), Mockito.any(QcStatus.class));
