4ca15a3c638af4277338ebd844a06a6886d6e294
David Streid <david.streid.nyc@gmail.com>
2020-03-12 10:27:10 -0400

    Refactor: Removing unused fields/methods from SampleTracker

--- src/main/java/org/mskcc/limsrest/service/requesttracker/SampleTracker.java
+++ src/main/java/org/mskcc/limsrest/service/requesttracker/SampleTracker.java
@@ -1,32 +1,25 @@
 package org.mskcc.limsrest.service.requesttracker;
 
 import com.velox.api.datarecord.DataRecord;
-import com.velox.api.user.User;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class SampleTracker {
-    private static Log log = LogFactory.getLog(SampleTracker.class);
-
     Long sampleId;
     DataRecord record;
     boolean complete;
     Map<String, Step> stepMap;
     Map<Long, AliquotStageTracker> sampleGraph;
     Boolean failed;
-    private User user;
     private List<List<AliquotStageTracker>> paths;
     private Map<String, SampleStageTracker> stages;
-    public SampleTracker(DataRecord record, User user) {
+
+    public SampleTracker(DataRecord record) {
         this.record = record;
         this.sampleId = record.getRecordId();
-        this.user = user;
         this.complete = true;       // The sample is considered complete until a record is added that is not done
 
         this.sampleGraph = new HashMap<>();
@@ -80,34 +73,10 @@ public class SampleTracker {
         this.paths = paths;
     }
 
-    public void addPath(List<AliquotStageTracker> path) {
-        this.paths.add(path);
-    }
-
     public DataRecord getRecord() {
         return this.record;
     }
 
-    public void addChildrenToRecord(Long recordId, List<Long> childRecords) {
-        this.sampleGraph.get(recordId).addChildren(childRecords);
-    }
-
-    /**
-     * Returns all paths that have a size greater than the input index
-     *
-     * @param paths
-     * @param idx
-     * @return
-     */
-    private List<List<Map<String, String>>> getRemainingPaths(List<List<Map<String, String>>> paths, int idx) {
-        List<List<Map<String, String>>> remainingPaths = new ArrayList<>();
-        for (List<Map<String, String>> path : paths) {
-            if (path.size() > idx) {
-                remainingPaths.add(path);
-            }
-        }
-        return remainingPaths;
-    }
 
     /**
      * Needs to be converted into a map to be returned in service response
