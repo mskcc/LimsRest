293e70c4934f519b2ee565b0f323882e79a50adf
David Streid <david.streid.nyc@gmail.com>
2020-06-09 16:29:46 -0400

    "Nano string" -> "NanoString"

--- src/main/java/org/mskcc/limsrest/util/StatusTrackerConfig.java
+++ src/main/java/org/mskcc/limsrest/util/StatusTrackerConfig.java
@@ -29,7 +29,7 @@ public class StatusTrackerConfig {
     public static final String STAGE_COVID_19_ASSAY = "COVID-19 Assay";
     public static final String STAGE_BATCH_PLANNING = "Batch Planning";
     public static final String STAGE_SAMPLE_ALIQUOTS = "Create Sample Aliquots";
-    public static final String STAGE_NANO_STRING = "Nano string";
+    public static final String STAGE_NANOSTRING = "NanoString";
     public static final String STAGE_STR_PCR = "STR PCR";
     public static final String STAGE_STR_ANALYSIS = "STR Analysis";
     public static final String STAGE_SNP_FINGERPRINTING = "SNP Fingerprinting";
@@ -73,7 +73,7 @@ public class StatusTrackerConfig {
             STAGE_COVID_19_ASSAY,
             STAGE_BATCH_PLANNING,
             STAGE_SAMPLE_ALIQUOTS,
-            STAGE_NANO_STRING,
+            STAGE_NANOSTRING,
             STAGE_STR_PCR,
             STAGE_STR_ANALYSIS,
             STAGE_SNP_FINGERPRINTING,
