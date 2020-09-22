156e5323622e9d35840b85bab8ba9b484d06581c
David Streid <david.streid.nyc@gmail.com>
2020-09-22 08:57:14 -0400

    /getIgoRequests

---
+++ src/main/java/org/mskcc/limsrest/controller/GetIgoRequests.java
@@ -0,0 +1,77 @@
+package org.mskcc.limsrest.controller;
+
+import com.velox.api.datarecord.IoError;
+import com.velox.api.datarecord.NotFound;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mskcc.limsrest.ConnectionLIMS;
+import org.mskcc.limsrest.ConnectionPoolLIMS;
+import org.mskcc.limsrest.service.GetDelivered;
+import org.mskcc.limsrest.service.GetIgoRequestsTask;
+import org.mskcc.limsrest.service.GetRequestTrackingTask;
+import org.mskcc.limsrest.service.RequestSummary;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.servlet.http.HttpServletRequest;
+import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Future;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static org.mskcc.limsrest.util.Utils.getResponseEntity;
+
+@RestController
+@RequestMapping("/")
+public class GetIgoRequests {
+    private static Log log = LogFactory.getLog(GetRecentDeliveries.class);
+    private final ConnectionLIMS conn;
+
+    public GetIgoRequests(ConnectionLIMS conn) {
+        this.conn = conn;
+    }
+
+    @GetMapping("/getIgoRequests")
+    public ResponseEntity<Map<String, Object>> getContent(@RequestParam(value = "days", defaultValue = "7") String days,
+                                                          @RequestParam(value = "complete", defaultValue = "true") String complete,
+                                                          HttpServletRequest request) {
+        String.format("Starting /getIgoRequests?days=%s&igoComplete=%s client IP: %s",
+                days, complete, request.getRemoteAddr());
+
+        Map<String, Object> resp = new HashMap<>();
+
+        Long numDays = 0L;
+        try {
+            numDays = parseLong(days);
+        } catch ( NumberFormatException e ){
+            String clientMessage = String.format("Couldn't process input days: %s", days);
+            log.error(String.format("%s. Error: %s", clientMessage, e.getMessage()));
+            resp.put("message", clientMessage);
+            resp.put("status", "Failed");
+            return getResponseEntity(resp, HttpStatus.BAD_REQUEST);
+        }
+
+        Boolean igoComplete = Boolean.parseBoolean(complete);
+        if(numDays < 0){
+            String clientMessage = String.format("Requested days must be greater than 0. Received Days: %s", days);
+            resp.put("message", clientMessage);
+            resp.put("status", "Failed");
+            return getResponseEntity(resp, HttpStatus.BAD_REQUEST);
+        }
+
+        GetIgoRequestsTask task = new GetIgoRequestsTask(numDays, igoComplete);
+        List<RequestSummary> requests = task.execute(this.conn.getConnection());
+        resp.put("status", "Success");
+        resp.put("requests", requests);
+        return getResponseEntity(resp, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/org/mskcc/limsrest/service/GetIgoRequestsTask.java
@@ -0,0 +1,73 @@
+package org.mskcc.limsrest.service;
+
+import com.velox.api.datarecord.DataRecord;
+import com.velox.api.datarecord.IoError;
+import com.velox.api.datarecord.NotFound;
+import com.velox.api.user.User;
+import com.velox.sapioutils.client.standalone.VeloxConnection;
+import com.velox.sloan.cmo.recmodels.RequestModel;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mskcc.limsrest.util.Utils.*;
+
+public class GetIgoRequestsTask extends LimsTask {
+    private static Log log = LogFactory.getLog(GetIgoRequestsTask.class);
+
+    private Long days;
+    private Boolean igoComplete;
+
+    public GetIgoRequestsTask(Long days, Boolean igoComplete){
+        this.days = days;
+        this.igoComplete = igoComplete;
+    }
+
+    private long getSearchPoint() {
+        long now = System.currentTimeMillis();
+        long offset = days * 24 * 60 * 60 * 1000;
+        return now - offset;
+    }
+
+    private String getQuery() {
+        long searchPoint = getSearchPoint();
+        if(this.igoComplete){
+            return String.format("%s > %d OR %s > %d", RequestModel.RECENT_DELIVERY_DATE, searchPoint,
+                    RequestModel.COMPLETED_DATE, searchPoint);
+        }
+        return String.format("%s IS NULL AND %s IS NULL", RequestModel.RECENT_DELIVERY_DATE, RequestModel.COMPLETED_DATE);
+    }
+
+    @PreAuthorize("hasRole('READ')")
+    @Override
+    public List<RequestSummary> execute(VeloxConnection conn) {
+        User user = conn.getUser();
+        String query = getQuery();
+        List<DataRecord> records = new ArrayList<>();
+        try {
+            records = conn.getDataRecordManager().queryDataRecords(RequestModel.DATA_TYPE_NAME, query, user);
+        } catch ( IoError | RemoteException | NotFound e){
+            log.error(String.format("Failed to query DataRecords w/ query: %s", query));
+            return new ArrayList<>();
+        }
+
+        // Transform requests into a redacted API response
+        List<RequestSummary> requests = new ArrayList<>();
+
+        for (DataRecord request : records) {
+            String requestId = getRecordStringValue(request, RequestModel.REQUEST_ID, user);
+            RequestSummary rs = new RequestSummary(requestId);
+            rs.setInvestigator(getRecordStringValue(request, RequestModel.INVESTIGATOR, user));
+            rs.setPi(getRecordStringValue(request, RequestModel.LABORATORY_HEAD, user));
+            rs.setRequestType(getRecordStringValue(request, RequestModel.REQUEST_NAME, user));
+            rs.setReceivedDate(getRecordLongValue(request, RequestModel.RECEIVED_DATE, user));
+            requests.add(rs);
+        }
+
+        return requests;
+    }
+}
