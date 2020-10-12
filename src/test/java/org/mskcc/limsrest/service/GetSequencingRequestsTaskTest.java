46534b18a5528321e442b70a82750663ae659a02
David Streid <david.streid.nyc@gmail.com>
2020-10-12 09:34:45 -0400

    Fixing test

--- src/test/java/org/mskcc/limsrest/service/GetSequencingRequestsTaskTest.java
+++ src/test/java/org/mskcc/limsrest/service/GetSequencingRequestsTaskTest.java
@@ -1,32 +1,35 @@
 package org.mskcc.limsrest.service;
 
+import com.velox.api.datarecord.DataRecord;
 import com.velox.api.datarecord.DataRecordManager;
 import com.velox.api.user.User;
 import com.velox.sapioutils.client.standalone.VeloxConnection;
+import com.velox.sloan.cmo.recmodels.FlowCellLaneModel;
+import com.velox.sloan.cmo.recmodels.RequestModel;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import javax.validation.constraints.AssertTrue;
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
+import java.util.Arrays;
 
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyList;
 import static org.mockito.Mockito.when;
 
 public class GetSequencingRequestsTaskTest {
-
-    @Before
-    public void setup() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
     @Test
-    public void isIMPACTOrHEMEPACT() {
-        GetSequencingRequestsTask task = new GetSequencingRequestsTask(0L, Boolean.TRUE, Boolean.TRUE);
+    public void queriesRequestsWithFlowCellSamples() {
+        Long numDays = 7L;
+        Boolean igoComplete = Boolean.TRUE;
+
+        GetSequencingRequestsTask task = new GetSequencingRequestsTask(numDays, igoComplete);
         VeloxConnection conn = Mockito.mock(VeloxConnection.class);
         User user = Mockito.mock(User.class);
+        DataRecord sample = Mockito.mock(DataRecord.class);
         DataRecordManager dataRecordManager = Mockito.spy(DataRecordManager.class);
         Mockito.doReturn(user).when(conn).getUser();
         Mockito.doReturn(dataRecordManager).when(conn).getDataRecordManager();
@@ -35,20 +38,33 @@ public class GetSequencingRequestsTaskTest {
             when(dataRecordManager.queryDataRecords(
                     Mockito.anyString(),
                     Mockito.anyString(),
-                    user
+                    Mockito.any(User.class)
             )).thenReturn(new ArrayList<>());
+
+            when(dataRecordManager.getParentsOfType(
+                    Mockito.anyListOf(DataRecord.class),
+                    Mockito.anyString(),
+                    Mockito.any(User.class)
+            )).thenReturn(Arrays.asList(Arrays.asList(sample)));
+
+            when(sample.getStringVal(
+                    Mockito.anyString(),
+                    Mockito.any(User.class)
+            )).thenReturn("TEST_REQUEST");
         } catch (Exception e) {
+            assertTrue(String.format("Failed to mock: %s", e.getMessage()), false);
         }
 
         task.execute(conn);
 
         try {
-            Mockito.verify(dataRecordManager, Mockito.times(5)).queryDataRecords(
+            Mockito.verify(dataRecordManager, Mockito.times(2)).queryDataRecords(
                     Mockito.anyString(),
                     Mockito.anyString(),
-                    user
+                    Mockito.any(User.class)
             );
         } catch (Exception e) {
+            assertTrue(String.format("Mockito verify failed: %s", e.getMessage()), false);
         }
     }
 }
