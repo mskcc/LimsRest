aa66fdf55af884c4de8c2725200ef5fd347b2535
David Streid <david.streid.nyc@gmail.com>
2020-12-01 17:40:34 -0500

    %f for doubles

--- src/main/java/org/mskcc/limsrest/service/PromoteBanked.java
+++ src/main/java/org/mskcc/limsrest/service/PromoteBanked.java
@@ -131,7 +131,7 @@ public class PromoteBanked extends LimsTask {
                 coverageTarget = 500;
                 requestedReads = 20.0;
             }
-            log.info(String.format("Overriding Heme Request (Recipe: %s) - Coverage Target: %d, Requested Reads: %d",
+            log.info(String.format("Overriding Heme Request (Recipe: %s) - Coverage Target: %d, Requested Reads: %f",
                     recipe, coverageTarget, requestedReads));
         } else if (recipe.contains("IMPACT")) { //'M-IMPACT_v1', 'IMPACT468'
             if (normal) {
@@ -141,7 +141,7 @@ public class PromoteBanked extends LimsTask {
                 coverageTarget = 500;
                 requestedReads = 14.0;
             }
-            log.info(String.format("Overriding IMPACT Request (Recipe: %s) - Coverage Target: %d, Requested Reads: %d",
+            log.info(String.format("Overriding IMPACT Request (Recipe: %s) - Coverage Target: %d, Requested Reads: %f",
                     recipe, coverageTarget, requestedReads));
         } else if (recipe.contains("ACCESS")) {
             coverageTarget = 1000;
@@ -150,7 +150,7 @@ public class PromoteBanked extends LimsTask {
             } else {
                 requestedReads = 60.0;
             }
-            log.info(String.format("Overriding ACCESS Request (Recipe: %s) - Coverage Target: %d, Requested Reads: %d",
+            log.info(String.format("Overriding ACCESS Request (Recipe: %s) - Coverage Target: %d, Requested Reads: %f",
                     recipe, coverageTarget, requestedReads));
         }
         coverageReadsMap.put("CoverageTarget", coverageTarget);
