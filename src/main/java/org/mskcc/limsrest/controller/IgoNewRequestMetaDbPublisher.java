2303a168f66361dac95579b860f599b0eae6fee3
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2021-01-12 16:51:25 -0500

    Added code to fetch project id by request id
    This logic was added in case it is not possible to pass the project id to this endpoint
    from within the MarkDelivery context that this endpoint is being hooked into.
    
    If we CAN pass the project id then we can remove the code which fetches a project summary
    given a request id. If not then we can keep it as is.
    
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

---
+++ src/main/java/org/mskcc/limsrest/controller/IgoNewRequestMetaDbPublisher.java
@@ -0,0 +1,166 @@
+package org.mskcc.limsrest.controller;
+
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Future;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mskcc.cmo.messaging.Gateway;
+import org.mskcc.cmo.shared.SampleManifest;
+import org.mskcc.limsrest.ConnectionLIMS;
+import org.mskcc.limsrest.ConnectionPoolLIMS;
+import org.mskcc.limsrest.service.GetProjectDetails;
+import org.mskcc.limsrest.service.GetRequestSamplesTask;
+import org.mskcc.limsrest.service.GetSampleManifestTask;
+import org.mskcc.limsrest.service.ProjectSummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+/**
+ * Publishes CMO requests as package to MetaDB with NATS messaging.
+ * Published package includes request ID and all samples for request.
+ * @author ochoaa
+ */
+@ComponentScan("org.mskcc.cmo.messaging")
+@RestController
+@RequestMapping("/")
+public class IgoNewRequestMetaDbPublisher {
+
+    @Value("${nats.publisher_topic}")
+    private String IGO_NEW_REQUEST_TOPIC;
+
+    @Autowired
+    private Gateway messagingGateway;
+
+    private final GetProjectDetails task = new GetProjectDetails();
+    private Log log = LogFactory.getLog(IgoNewRequestMetaDbPublisher.class);
+    private final ConnectionLIMS conn;
+    private final ConnectionPoolLIMS connPool;
+
+    public IgoNewRequestMetaDbPublisher(ConnectionLIMS conn, ConnectionPoolLIMS connPool) throws Exception {
+        this.conn = conn;
+        this.connPool = connPool;
+        if (messagingGateway == null) {
+            log.error("Error establishing connection to NATS server, messagingGateway is NULL");
+        } else {
+            if (!messagingGateway.isConnected()) {
+                messagingGateway.connect();
+            }
+        }
+    }
+
+    @GetMapping("/publishIgoRequestToMetaDb")
+    public void getContent(@RequestParam(value = "requestId") String requestId,
+            @RequestParam(value = "projectId") String projectId, HttpServletRequest request) {
+        log.info("/publishIgoRequestToMetaDb for request: " + requestId + " " + request.getRemoteAddr());
+
+        if (!Whitelists.requestMatches(requestId)) {
+            log.error("FAILURE: requestId is not using a valid format.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "FAILURE: requestId is not using a valid format.");
+        }
+
+        //TODO: If possible it would be ideal if project id can simply be provided
+        // when calling the endpoint. This block of code was added in case this
+        // request is not possible to hook into the MarkDelivery logic. If project id
+        // CAN be provided when calling this endpoint then we can remove this block of code
+
+        // if project id is null then get project summary for request and extract project id
+        if (projectId == null || projectId.isEmpty()) {
+            task.init(requestId);
+            Future<Object> result = connPool.submitTask(task);
+            ProjectSummary ps = new ProjectSummary();
+            try{
+                ps = (ProjectSummary)result.get();
+                projectId = ps.getProjectId();
+            } catch(Exception e){
+                ps.setCmoProjectId(e.getMessage());
+            }
+        }
+        List<SampleManifest> sampleManifestList = getSampleManifestListByRequestId(requestId);
+        publishIgoNewRequestToMetaDb(projectId, requestId, sampleManifestList);
+    }
+
+    /**
+     * Returns list of sample manifest instances given a request id.
+     * @param requestId
+     * @return
+     */
+    private List<SampleManifest> getSampleManifestListByRequestId(String requestId) {
+        // fetch samples for request
+        GetRequestSamplesTask.RequestSampleList sl = null;
+        try {
+            GetRequestSamplesTask t = new GetRequestSamplesTask(requestId, conn);
+             sl = t.execute();
+            if ("NOT_FOUND".equals(sl.requestId)) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, requestId + " Request Not Found");
+            }
+        } catch (Exception e) {
+            log.error(e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+
+        // construct list of igo id strings for the 'GetSampleManifestTask'
+        List<String> igoIds = new ArrayList<>();
+        for (GetRequestSamplesTask.RequestSample rs : sl.getSamples()) {
+            igoIds.add(rs.getIgoSampleId());
+        }
+
+        // fetch list of sample manifests for request sample (igo)ids
+        List<SampleManifest> sampleManifestList = null;
+        GetSampleManifestTask sampleManifest = new GetSampleManifestTask((String[]) igoIds.toArray(), conn);
+        GetSampleManifestTask.SampleManifestResult result = sampleManifest.execute();
+        if (result == null) {
+            log.error("Sample Manifest generation failed for: " + igoIds.toString());
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        } else if (result.error == null) {
+            log.info("Returning n rows: " + result.smList.size());
+            sampleManifestList = result.smList;
+        } else {
+            log.error("Sample Manifest generation failed with error: " + result.error);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, result.error);
+        }
+        return sampleManifestList;
+    }
+
+    /**
+     * Packages message for CMO MetaDB and publishes to MetaDB NATS server.
+     * @param projectId
+     * @param requestId
+     * @param sampleManifestList
+     */
+    private void publishIgoNewRequestToMetaDb(String projectId, String requestId, List<SampleManifest> sampleManifestList) {
+        // construct igo request entity to publish to metadb
+        Gson gson = new Gson();
+        Map<String, Object> igoRequestMap = new HashMap<>();
+        igoRequestMap.put("projectId", projectId);
+        igoRequestMap.put("requestId", requestId);
+        igoRequestMap.put("sampleManifestList", sampleManifestList);
+        String igoRequestJson = gson.toJson(igoRequestMap);
+        try {
+            log.debug("Publishing cmo request entity: " + igoRequestJson);
+            messagingGateway.publish(IGO_NEW_REQUEST_TOPIC, igoRequestJson);
+        } catch (Exception e) {
+            String metadbErrorMsg = "Error during attempt to publish new request entity to MetaDB with request id: " + requestId;
+            log.error(metadbErrorMsg);
+            if (log.isDebugEnabled()) {
+                StringBuilder builder = new StringBuilder(metadbErrorMsg);
+                builder.append("\n****FAILED IGO_NEW_REQUEST MESSAGE****\n")
+                        .append(igoRequestJson)
+                        .append("\n****************\n");
+                log.debug(builder);
+            }
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, metadbErrorMsg, e);
+        }
+    }
+}
