e69f9a745a6117a67a02dcad48c1d973b87071f7
Mirhaj <f.mirhaj@gmail.com>
2023-08-09 12:02:29 -0400

    Removing extra logging.

--- src/main/java/org/mskcc/limsrest/service/GetTenXSampleInfoTask.java
+++ src/main/java/org/mskcc/limsrest/service/GetTenXSampleInfoTask.java
@@ -43,10 +43,8 @@ public class GetTenXSampleInfoTask {
             User user = vConn.getUser();
 
             requestId = requestId.split("_")[0];
-            log.info("Core requestId = " + requestId);
 
             List<DataRecord> requests = dataRecordManager.queryDataRecords("Request", "requestId like '" + requestId + "%' AND RequestName like '%10X%'", user);
-            log.info("Related requests list size = " + requests.size());
             List<Map<String, List<String>>> samplesRecipes = new LinkedList<>();
 
             for (DataRecord request : requests) {
@@ -54,7 +52,7 @@ public class GetTenXSampleInfoTask {
                 DataRecord[] listOfSamples = request.getChildrenOfType("Sample", user);
                 Map<String, List<String>> samplesToRecipes = new HashMap<>();
                 for (DataRecord sample : listOfSamples) {
-                    List<String> samplesInfo = new LinkedList<>(); // sample name and recipe
+                    List<String> samplesInfo = new LinkedList<>(); // iLab request, sample name and recipe
                     String recipe = sample.getStringVal("Recipe", user);
                     String sampleName = sample.getStringVal("OtherSampleId", user);
                     log.info("recipe of sample " + sample.getStringVal("SampleId", user) + " is: " + recipe);
