dd33c6374beb66966e1a6bd3ecd34bc13a331098
Mirhaj <mirhajf@lski2475.mskcc.org>
2024-10-09 10:35:00 -0400

    Fixing cell hashing pull into lims

--- src/main/java/org/mskcc/limsrest/service/UpdateFromILabsTask.java
+++ src/main/java/org/mskcc/limsrest/service/UpdateFromILabsTask.java
@@ -203,7 +203,7 @@ public class UpdateFromILabsTask {
                 }
 
                 if (field2val.containsKey("TREATMENT")) {
-                    if ((field2val.get("TREATMENT").toLowerCase().contains("cell") || (field2val.get("TREATMENT").toLowerCase().contains("hashing")) && field2val.get("TREATMENT").toLowerCase().contains("barcoding")) {
+                    if ((field2val.get("TREATMENT").toLowerCase().contains("cell") || (field2val.get("TREATMENT").toLowerCase().contains("hashing")) && field2val.get("TREATMENT").toLowerCase().contains("barcoding"))) {
                         requestFields.put("Treatment", "Cell Hashing, Feature Barcoding");
                     }
                     else if (field2val.get("TREATMENT").toLowerCase().contains("barcoding")) {
